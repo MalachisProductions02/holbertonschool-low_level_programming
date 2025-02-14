598a17e0c6006c732af21301f59a1ddbded6dda5
MalachisProductions02 <maalchisproductionsnooficial@gmail.com>
2025-02-14 01:55:55 +0000

    5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - Inicio funci√n printf
+ *
+ * Return: Siempre 0, fin de la funci√n
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
