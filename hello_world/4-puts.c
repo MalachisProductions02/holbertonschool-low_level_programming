7209332890bd05fb7b965b7cad3ad4e2625ec9b4
MalachisProductions02 <maalchisproductionsnooficial@gmail.com>
2025-02-14 01:44:54 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <studio.h>
+#include <stdio.h>
 
 
 /**
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return();
 }
