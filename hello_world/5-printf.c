ce6f9e99670547a1127cf8af1ed3dfb167f4aecd
MalachisProductions02 <maalchisproductionsnooficial@gmail.com>
2025-02-14 02:16:05 +0000

    5-printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- * main - Inicio funci√n printf
+ * main - funcion inicial
  *
- * Return: Siempre 0, fin de la funci√n
+ * Return: 0
  *
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
