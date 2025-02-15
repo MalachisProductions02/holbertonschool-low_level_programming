6cf247b5bc782e8a007a71e4a203661e88fa8420
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-02-15 22:54:08 +0000

    4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - Revisa que el caracter sea una letra
+ * @c: Caracter a checar
+ *
+ * Return: 1 si es letra o 0 si es otra cosa XD
+ *
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c<= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -10,6 +10,7 @@ void print_alphabet_x10(void);
 void print_alphabet(void);
 
 int _putchar(char);
-int _islower(int); 
+int _islower(int);
+int _isalpha(int);
 
 #endif /* MAIN_H */
