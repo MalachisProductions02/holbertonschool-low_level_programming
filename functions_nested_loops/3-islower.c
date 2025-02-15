082b3d45f26712710514da2c5d20f62be00919a4
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-02-15 22:22:27 +0000

    3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - Checker que identifica un caracter si es minuscula
+ *
+ * Return: 1 si es minuscula, 0 si no lo es
+ *
+ */
+int _islower(int n)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -6,8 +6,10 @@
  *int prototypes (int)
  *
  */
-void print_alphabet_x10(void); 
+void print_alphabet_x10(void);
 void print_alphabet(void);
+
 int _putchar(char);
+int _islower(int); 
 
 #endif /* MAIN_H */
