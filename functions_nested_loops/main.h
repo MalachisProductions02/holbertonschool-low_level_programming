6fb68f95c77c673ce4e2e3853c0cacd7b722fd34
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-02-15 21:33:49 +0000

    main

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * main - Aqui van los prototipos, algo asi como una biblioteca
+ *void prototypes (void)
+ *int prototypes (int)
+ *
+ */
+int _putchar(char);
+
+#endif /* MAIN_H */
