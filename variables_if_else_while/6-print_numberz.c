4eb4ba3d2e02e5779718697be6f55375b2aa3bf2
MalachisProductios02 <malachisproductionsnooficial@gmail.com>
2025-02-15 03:29:59 +0000

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Imprimir numeros en base pero sin usar char XD
+ *
+ * Return: Always 0 :D
+ *
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
