ebca4622c3ea249cc3b65d60fe1695a30879fa3e
MalachisProductios02 <malachisproductionsnooficial@gmail.com>
2025-02-15 03:35:43 +0000

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Imprimir el alfabeto a reves JAJAAJA
+ *
+ * Return: I'm sure is 0 but I don't understand so much...
+ *
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
