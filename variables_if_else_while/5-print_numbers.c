9bd499f066de55bbe37bfc4a23f9ebce5dc50c6f
MalachisProductios02 <malachisproductionsnooficial@gmail.com>
2025-02-15 03:21:52 +0000

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Imprimir numeros en base 10 sin el 10 JAJAJAJAJAJ
+ *
+ * Return: I think is 0...I have doubths...
+ *
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; c <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
