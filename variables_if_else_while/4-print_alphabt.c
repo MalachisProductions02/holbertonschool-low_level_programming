af597a67e0361570c8171242ecc6b9e32cf22584
MalachisProductios02 <malachisproductionsnooficial@gmail.com>
2025-02-15 03:08:38 +0000

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Imprime el alfabeto sin e ni q
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+	}
+
+	putchar('\n');
+
+	return(0);
+}
