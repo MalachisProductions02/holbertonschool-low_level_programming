45a03b5a8ab77f886cbe14399a57c4148e0d0fda
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-02-16 00:19:18 +0000

    8

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -8,12 +8,13 @@
  */
 void print_alphabet_x10(void);
 void print_alphabet(void);
+void jack_bauer(void);
 
 int _putchar(char);
 int _islower(int);
 int _isalpha(int);
 int print_sign(int);
 int _abs(int);
-int print_last_digit(int); 
+int print_last_digit(int);
 
 #endif /* MAIN_H */
