cd57c0ab597e8baf9c292f2612ffa0bae935e753
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-02-17 20:47:44 -0600

    11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
+/**
+* print_to_98 - Imprime numeros hasta el 98
+*@n: Valor numerico
+*
+*Return: Always 0
+*/
 void print_to_98(int n)
 {
     if (n <= 98)
     {
         for (; n < 98; n++)
         {
-            if (n < 10)
-            {
-                _putchar('0' + n);
-            }
-            else
-            {
-                _putchar('0' + n / 10);
-                _putchar('0' + n % 10);
-            }
+            _putchar('0' + n / 10);
+            _putchar('0' + n % 10);
             _putchar(',');
             _putchar(' ');
         }
@@ -23,15 +22,8 @@ void print_to_98(int n)
     {
         for (; n > 98; n--)
         {
-            if (n < 10)
-            {
-                _putchar('0' + n);
-            }
-            else
-            {
-                _putchar('0' + n / 10);
-                _putchar('0' + n % 10);
-            }
+            _putchar('0' + n / 10);
+            _putchar('0' + n % 10);
             _putchar(',');
             _putchar(' ');
         }
