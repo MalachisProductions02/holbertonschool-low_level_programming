7e55b5e1cb35670026a23e3aa73c68b6c47770b0
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-02-18 01:30:39 +0000

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *
+ *  main - Suma de dos enteros
+ *  @a: Primer numero
+ *  @b: Segundo numero
+ *  
+ *  Return: La suma de a y b
+ *
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -17,5 +17,6 @@ int _isalpha(int);
 int print_sign(int);
 int _abs(int);
 int print_last_digit(int);
+int add(int, int);
 
 #endif /* MAIN_H */
