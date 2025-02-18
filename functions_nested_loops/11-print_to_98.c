b19165971ad71da1446db67d6c3fe4ded24b4fae
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-02-18 15:35:35 -0600

    11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,35 +8,57 @@
 */
 void print_to_98(int n)
 {
-	while (n != 98)
+	if (n > 98)
 	{
-		if (n < 0)
+		while (n > 98)
 		{
-			_putchar('-');
-			if (n <= -10)
+			if (n < 0)
 			{
-				_putchar('0' + (-n / 10));
+				_putchar('-');
+				n = -n;
 			}
-			_putchar('0' + (-n % 10));
-		}
-		else
-		{
-			if (n >= 10)
+
+			if (n >= 100)
+			{
+				_putchar('0' + (n / 100));
+				_putchar('0' + ((n / 10) % 10));
+			}
+			else if (n >= 10)
 			{
 				_putchar('0' + (n / 10));
 			}
 			_putchar('0' + (n % 10));
-		}
 
-		_putchar(',');
-		_putchar(' ');
+			_putchar(',');
+			_putchar(' ');
 
-		if (n > 98)
-		{
 			n--;
 		}
-		else
+	}
+	else
+	{
+		while (n < 98)
 		{
+			if (n < 0)
+			{
+				_putchar('-');
+				n = -n;
+			}
+
+			if (n >= 100)
+			{
+				_putchar('0' + (n / 100));
+				_putchar('0' + ((n / 10) % 10));
+			}
+			else if (n >= 10)
+			{
+				_putchar('0' + (n / 10));
+			}
+			_putchar('0' + (n % 10));
+
+			_putchar(',');
+			_putchar(' ');
+
 			n++;
 		}
 	}
