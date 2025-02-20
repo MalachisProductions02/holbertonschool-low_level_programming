52da7262b6a7816f8871d59864df6f7504b6f810
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-02-19 21:39:31 -0600

    3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -8,12 +8,12 @@
  */
 int _islower(int n)
 {
-	if (n >= 'a' && n <= 'z')
+	if (n >= 'a' && n <= 'z') //Esto básicamente es un filtro que detecta si los caracteres estan en minúscula para regresar 1 (osea verdadero)
 	{
-		return (1);
+		return (1);  //Verdadero
 	}
 	else
 	{
-		return (0);
+		return (0);  //Si está en mayúsculas o es otro caracter, regresa cero (osea falso)
 	}
 }
