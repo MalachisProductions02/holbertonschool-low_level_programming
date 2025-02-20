e05dd45d1adf4cd3762dad7fec6f9b79be8b6f4e
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-02-19 21:00:06 -0600

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle"); //Imprime en la consola el mensaje
 	return (0);
 }
