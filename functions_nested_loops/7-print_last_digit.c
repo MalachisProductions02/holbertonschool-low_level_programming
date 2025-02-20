f6b772f7b392dd87bf74043db2373f1b20f6c419
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-02-19 21:48:55 -0600

    7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,13 +8,13 @@
  */
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+	int last_digit = n % 10;  //Algo complejo...básicamente declara last_digit en n % 10 que es una unidad
 
 	if (last_digit < 0)
 	{
-		last_digit = -last_digit;
+		last_digit = -last_digit;  //Si es menor que cero, cambia la jerarquía de la unidad
 	}
 
-	_putchar(last_digit + '0');
-	return (last_digit);
+	_putchar(last_digit + '0');  //Imprime el dígito PERO le suma el ASCII de cero para evitar que salgan otros caracteres que no sean números
+	return (last_digit);  //Regresa el dígito
 }
