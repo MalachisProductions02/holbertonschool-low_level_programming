471aff24d3401419c4db5fdebd34b0a6f3b36f24
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-02-19 21:00:47 -0600

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -9,6 +9,6 @@
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n"); //Es el print común y corriente de Python, sirve para imprimir un mensaje
 return (0);
 }
