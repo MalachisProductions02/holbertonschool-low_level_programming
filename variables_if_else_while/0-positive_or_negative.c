050c6099b47cf588945e9484164bc4e1eba4c4c7
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-02-19 21:12:41 -0600

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,20 +12,20 @@ int main(void)
 {
 	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	srand(time(0)); //Comando para randomizar valores
+	n = rand() - RAND_MAX / 2; //Prender el randomizador
 
 	if (n > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is positive\n", n); //Detecta si el valor es positivo y lo imprime
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n", n);
+		printf("%d is zero\n", n); //Detecta si el valor es cero y lo imprime
 	}
 	else
 	{
-		printf("%d is negative\n", n);
+		printf("%d is negative\n", n); //Detecta si el valor es negativo y lo imprime
 	}
 
 	return (0);
