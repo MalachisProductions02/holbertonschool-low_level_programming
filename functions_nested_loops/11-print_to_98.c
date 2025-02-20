2547a3d7015de6fbf23075981472fdeae0004ade
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-02-20 02:36:49 +0000

    11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,75 +1,27 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - Imprime números en distintas secuencias hasta 98
  * @n: Valor numérico
- *@num: Valor numérico
- *@i: Valor numérico
- *@first: Valor numérico
  *
  * Return: Always 0
  */
 void print_to_98(int n)
 {
-	(void)n;
-
-	int num, i; first;
-	int sequences[4] = {0, 98, 111, -10};
-
-	for (i = 0; i < 4; i++)
+	if (n <= 98)
 	{
-		num = sequences[i];
-		first = 1;
-
-		while (num != 98)
+		for (; n < 98; n++)
 		{
-			if (!first)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			first = 0;
-
-			if (num < 0)
-			{
-				_putchar('-');
-				if (num <= -10)
-				{
-					_putchar('0' + (-num / 10));
-				}
-				_putchar('0' + (-num % 10));
-			}
-			else
-			{
-				if (num >= 100)
-				{
-					_putchar('0' + (num / 100));
-					_putchar('0' + ((num / 10) % 10));
-				}
-				else if (num >= 10)
-				{
-					_putchar('0' + (num / 10));
-				}
-				_putchar('0' + (num % 10));
-			}
-
-			if (num > 98)
-			{
-				num--;
-			}
-			else
-			{
-				num++;
-			}
+			printf("%d, ", n);
 		}
-
-		if (!first)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-		_putchar('9');
-		_putchar('8');
-		_putchar('\n');
 	}
+	else
+	{
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	printf("98\n");
 }
