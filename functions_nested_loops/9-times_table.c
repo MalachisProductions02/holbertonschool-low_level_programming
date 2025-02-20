e8ea10f3c14d12dc1078c3651a159bcead8f4212
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-02-19 21:57:31 -0600

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,31 +9,31 @@ void times_table(void)
 {
 	int n, c, result;
 
-	for (n = 0; n < 10; n++)
+	for (n = 0; n < 10; n++)  //n declarada en cero, si n es menor que 10, entonces se le suma uno
 	{
-		for (c = 0; c < 10; c++)
+		for (c = 0; c < 10; c++)  //c declarada en cero, si c es menor que 10, entonces se le suma uno
 		{
-			result = n * c;
+			result = n * c;  //Se multiplica n por c
 
-			if (c != 0)
+			if (c != 0)  //Si c no es cero, entonces...
 			{
-				_putchar(',');
-				_putchar(' ');
+				_putchar(',');  //Imprime la coma
+				_putchar(' ');  //Imprime el espacio vacío
 
-				if (result < 10)
+				if (result < 10)  //Detecta que el resultado sea menor que 10
 				{
-					_putchar(' ');
+					_putchar(' ');  //Si lo es, entonces imprimirá un espacio vacío
 				}
 			}
 
-			if (result >= 10)
+			if (result >= 10)  //Si el resultado es mayor o igual a 10...
 			{
-				_putchar('0' + (result / 10));
-				_putchar('0' + (result % 10));
+				_putchar('0' + (result / 10));  //Suma el ASCII de cero a la conversión a decenas
+				_putchar('0' + (result % 10));  //Suma el ASCII de cero a la conversión a unidades
 			}
 			else
 			{
-				_putchar('0' + result);
+				_putchar('0' + result);  //Suma el ASCII de cero al resultado para asegurar que sea un número y no otro caracter
 			}
 		}
 		_putchar('\n');
