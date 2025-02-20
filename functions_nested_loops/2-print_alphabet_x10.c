1579bf820ae69f42ea9d6f360674601bc3072691
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-02-19 21:35:36 -0600

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_alphabet_x10 - Imprime el alfabeto 10 veces
  *
@@ -11,13 +10,13 @@ void print_alphabet_x10(void)
 	int n;
 	char c;
 
-	for (n = 0; n < 10; n++)
+	for (n = 0; n < 10; n++)  //Determinamos n como 0 y si es menor que 10, se le suma uno
 	{
-		for (c = 'a'; c <= 'z'; c++)
+		for (c = 'a'; c <= 'z'; c++)  //Declaramos c como a y se le suma uno cada vez que imprime un caracter
 		{
-			_putchar(c);
+			_putchar(c);  //Impresión de los caracteres de a a la z hasta que n sea 10
 		}
 
-		_putchar('\n');
+		_putchar('\n');  //Salto de línea
 	}
 }
