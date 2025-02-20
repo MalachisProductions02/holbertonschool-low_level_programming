1a47b3390fe8fcf344d3a5228c86c8f9c0072d00
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-02-19 21:32:36 -0600

    11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,19 +9,19 @@
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
+	if (n <= 98)  //Detecta si el valor es menor o igual a 98
 	{
-		for (; n < 98; n++)
+		for (; n < 98; n++)  //';' al inicio de un bucle for sirve para indicar un valor vacío
 		{
-			printf("%d, ", n);
+			printf("%d, ", n);  //Si el valor es menor que 98, suma uno a n y lo imprime con una coma y espacio vacío
 		}
 	}
 	else
 	{
-		for (; n > 98; n--)
+		for (; n > 98; n--)  // Detecta si el valor es mayor que 98 y le resta uno
 		{
-			printf("%d, ", n);
+			printf("%d, ", n);  //Imprime el valor n con una coma y espacio vacío
 		}
 	}
-	printf("98\n");
+	printf("98\n");  //Imprime el número 98 con un salto de línea siempre que termine una serie
 }
