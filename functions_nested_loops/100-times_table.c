831f3a70acfa1bb59f7138fa981d26b7b96a230f
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-02-21 20:56:38 -0600

    100

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -15,47 +15,49 @@ void print_times_table(int n)
 
     if (n < 0 || n > 15)
     {
-       return;
+        return;
     }
 
     for (i = 0; i <= n; i++)
     {
-      for (j = 0; j <= n; j++)
-      {
-        prod = i * j;
+          for (j = 0; j <= n; j++)
+          {
+               prod = i * j;
 
-        if (prod < 10)
-        {
-          c = prod + '0';
-          write(1, &c, 1);
-        }
-        else if (prod < 100)
-        {
-          c = (prod / 10) + '0';
-          write(1, &c, 1);
-          c = (prod % 10) + '0';
-          write(1, &c, 1);
-        }
-        else
-        {
-          c = (prod / 100) + '0';
-          write(1, &c, 1);
-          c = ((prod / 10) % 10) + '0';
-          write(1, &c, 1);
-          c = (prod % 10) + '0';
-          write(1, &c, 1);
-        }
+               if (prod < 10)
+               {
+                    c = prod + '0';
+                    write(1, " ", 3);
+                    write(1, &c, 1);
+                }
+                else if (prod < 100)
+                {
+                    c = (prod / 10) + '0';
+                    write(1, " ", 2);
+                    write(1, &c, 1);
+                    c = (prod % 10) + '0';
+                    write(1, &c, 1);
+                }
+                else
+                {
+                    c = (prod / 100) + '0';
+                    write(1, &c, 1);
+                    c = ((prod / 10) % 10) + '0';
+                    write(1, &c, 1);
+                    c = (prod % 10) + '0';
+                    write(1, &c, 1);
+                }
 
-        if (j != n)
-        {
-          c = ',';
-          write(1, &c, 1);
-          c = ' ';
-          write(1, &c, 1);
-        }
-      }
+                if (j != n)
+                {
+                    c = ',';
+                    write(1, &c, 1);
+                    c = ' ';
+                    write(1, &c, 1);
+                }
+          }
 
-      c = '\n';
-      write(1, &c, 1);
-    }
+          c = '\n';
+          write(1, &c, 1);
+     }
 }
