758e516baa47de552d3e01f930647175909f2af9
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-02-22 01:12:19 +0000

    102-print_comb5

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Imprime todas las combinaciones posibles de dos numeros
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = '0'; a <= '98'; a++)
+	{
+		for (b = '0'; b <= '99'; b++)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+
+			if (a != '98' || b != '99')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
