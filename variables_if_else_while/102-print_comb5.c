65c0ae78af9642f6dad5339f1a85f2fdf0ace383
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-02-22 01:21:09 +0000

    102-print_comb5

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	int a, b;
 
-	for (a = '0'; a <= '98'; a++)
+	for (a = 0; a <= 98; a++)
 	{
-		for (b = '0'; b <= '99'; b++)
+		for (b = 0; b <= 99; b++)
 		{
 			putchar((a / 10) + '0');
 			putchar((a % 10) + '0');
@@ -20,7 +20,7 @@ int main(void)
 			putchar((b / 10) + '0');
 			putchar((b % 10) + '0');
 
-			if (a != '98' || b != '99')
+			if (a != 98 || b != 99)
 			{
 				putchar(',');
 				putchar(' ');
