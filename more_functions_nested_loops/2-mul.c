53e237b9de353a83f2d787f7b5661ed15b9f2331
MalachisProductions02 <maalchisproductionsnooficial@gmail.com>
2025-02-25 01:20:14 +0000

    2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - MUltiplica dos numeros
+ * @a: Valor a ingresar
+ * @b: Valor a ingresar
+ * Return: Regresa el valor de la operacion
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
