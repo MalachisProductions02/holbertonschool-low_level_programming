8f5fcc4cac3f9a1e9203d432064ca626abcf61e5
MalachisProductions02 <maalchisproductionsnooficial@gmail.com>
2025-02-25 02:28:03 +0000

    4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_most_numbers - Imprime una serie numerica sin imprimir el 2 y el 4
- * @i: Valor a ingresar
+ * 
  * Return: Always 0 when is void function
  */
 void print_most_numbers(void)
