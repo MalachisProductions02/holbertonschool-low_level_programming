4dcdaede17c4f97b7c140635c220acd08eee75ae
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-02-25 22:37:20 +0000

    9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Serie numerica que cambia mul de 3 y 5 a fizz buzz
+ *
+ * Return_ Always 0
+ */
+int main(void)
+{
+	int i;
+
+	while (i++ < 100)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((i % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((i % 5) == 0)
+		{
+			if (i != 100)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("Buzz");
+			}
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+
+	printf("\n");
+	return(0);
+}
