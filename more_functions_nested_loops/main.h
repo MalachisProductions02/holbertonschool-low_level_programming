3dcc4e15f55f7bba507edae21d54d962de10a300
MalachisProductions02 <maalchisproductionsnooficial@gmail.com>
2025-02-25 01:30:51 +0000

    main

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -6,8 +6,11 @@
  *void prototypes (void)
  *int prototypes (int)
  */
+void print_numbers(void);
+
 int _isupper(int);
 int _isdigit(int);
 int mul(int, int);
+int _putchar(char);
 
 #endif /*MAIN_H*/
