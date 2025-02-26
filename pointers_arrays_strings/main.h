4f49048e46c6bb119b7ff85b421da2cf6261d596
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-02-25 21:15:26 -0600

    main

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -9,7 +9,7 @@
  */
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-void swap_char(int *a, int *b);
+void swap_char(char *a, char *b);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
