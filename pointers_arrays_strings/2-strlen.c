ab5d7d4d0d2e1a814b4367c52e4a9b30296535d6
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-02-26 01:16:02 +0000

    2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - Regresa el largo de una string
+ * @s: Parametro
+ * Return: Regresa lenght
+ */
+int _strlen(char *s)
+{
+	int count, inc;
+	inc = 0;
+
+	for (count  = 0; s[count] != '\0'; count++)
+	{
+		inc++;
+	}
+
+	return (inc);
+}
