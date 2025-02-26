d4763d07b274e77582750c3608d009e2c6e5300b
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-02-26 01:37:18 +0000

    3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - Imprime un string
+ * @str: Parametro
+ * 
+ * Description: Imprime un string
+ * Return: Regresa el numero de caracteres de un string
+ */
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str++);
+	}
+
+	_putchar('\n');
+}
