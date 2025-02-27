2477d6d17a6e25343e827ad508dab1125dca144e
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-02-27 23:31:27 +0000

    0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - concatena dos strings
+ * @dest: El destino
+ * @src: Source
+ * Return: Resultado de la funcion
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (src[j] = '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+
+	return dest;
+}
