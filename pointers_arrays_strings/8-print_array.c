0cb19cd53c14023bad275540e2dd0c3bde2fb5a5
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-02-27 01:14:37 +0000

    8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_array - Imprime n elementos de un listado
+ * @a: Parametro
+ * @n: Parametro del listado
+ * Return: Always 0
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != n - 1)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			print("%d", a[i]);
+		}
+	}
+	printf("\n");
+}
