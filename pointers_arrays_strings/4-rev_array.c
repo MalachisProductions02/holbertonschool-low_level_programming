c2a3e02ede58148bdbdd97a0bc346db10dec58be
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-02-28 02:02:22 +0000

    4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * rev_array - Invierte un listado de enteros
+ * @a: variable puntero del listado
+ * @n: Parametro
+ * Return: Nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int i, tmp;
+
+	for (i = 0, i < n / 2; i++)
+	{
+		tmp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 -i] = tmp;
+	}
+}
