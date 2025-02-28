1bb200d497a0be660a38e844d591a2672c39511e
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-02-28 00:04:03 +0000

    1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "mian.h"
+
+/**
+ * _strncat - Concatena dos cadenas de caracteres
+ * @dest: El destino
+ * @src: Origen
+ * @n: Parametro
+ * Return: resultado de _strncat
+ */
+char _strncat(char *dest, char *src, int n)
+{
+	int i = 0; j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}	
