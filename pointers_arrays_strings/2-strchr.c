2cfc1fc98aca5b4749d8bd69fed6652beb00c2ec
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-03-03 22:05:18 +0000

    2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - Localiza un caracter en un string
+ * @s: Puntero
+ * @c: Parametro
+ * Return: Puntero a la primera ocurrenca de c o s, sino NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+
+	if (*s == c)
+	{
+		return (s);
+	}
+
+	return NULL;
+}
