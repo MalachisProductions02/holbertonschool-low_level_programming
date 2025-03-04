7a3e5a171ccb4cb9af49b89d0f3b80a1daf15f58
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-03-04 23:40:26 +0000

    5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strstr - Encuentra la primera aparición en el string needle dentro de haystack
+ * @haystack: Strings principal
+ * @needle: Substring
+ * Return: Regresa Haystack
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *h, *n;
+
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
+
+	while (*haystack)
+	{
+		h = haystack;
+		n = needle;
+
+		while (*h && *n && (*h == *n))
+		{
+			h++;
+			n++;
+		}
+
+		if (*n == '\0')
+		{
+			return (haystack);
+		}
+		haystack++;
+	}
+	
+	return (NULL);
+}
