3d8ec4e9bf936829b27e0189bb3503ba360bd721
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-03-05 02:23:54 +0000

    main

---
+++ recursion/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+ * void prototype
+ * char prototype
+ * int prototype
+ * unsigned prototype
+ */
+void _puts_recursions(char *s);
+
+int _putchar(char);
+
+#endif /*MAIN_H*/
