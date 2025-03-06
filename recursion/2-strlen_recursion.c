ec8f16328ab2a9ca3176e0c7fcec4eb7d23c8b00
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-03-06 00:59:38 +0000

    2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Regresa el tamaño del string
+ * @s: Parametro
+ * Return: Resultado del string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
