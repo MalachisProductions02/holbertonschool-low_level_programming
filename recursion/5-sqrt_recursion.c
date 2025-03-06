dd2fa07d4a7a9cfe1a84a1dc26dc308630d08362
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-03-06 22:35:39 +0000

    5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _sqrt_helper - Funcion de ayuda para calcular raiz 
+ * @n: Parametro
+ * @guess: Segundo parametro
+ * Return: Regresa n
+ */
+int _sqrt_helper(int n, int guess)
+{
+	if (guess * guess == n)
+	{
+		return (guess);
+	}
+	if (guess * guess > n)
+	{
+		return (-1);
+	}
+	return (sqrt_helper(n, guess + 1));
+}
+
+/**
+ * _sqrt_recursio - Calcula la raiz de un numero
+ * @n: Parametro
+ * Return: Regresa n
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (_sqrt_recursion(n, 1));
+}
