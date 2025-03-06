19816fd31107d2d141a61fa4ff33e66122ec69aa
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-03-06 23:25:32 +0000

    6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * is_divisible - Revisa que n sea divisible
+ * @i: Parametro
+ * @n: Parametro a dar
+ * Return: Regresa n
+ */
+int is_divisible(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (0);
+	}
+	if (n % i == 0)
+	{
+		return (1);
+	}
+	return (is_divisible(n, i + 1));
+}
+
+/**
+ * is_prime_number - Identifica si n es numero primo
+ * @n: Parametro
+ * Return: Regresa n
+ */
+int is_prime_number(int n)
+{
+	if (n <= 0)
+	{
+		return (0);
+	}
+	return (is_divisible(n, 2) == 0 ? 1 : 0);
+}
