1f7335bc2edff40ef7d674e2313887bae41e3b01
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-03-07 18:45:43 +0000

    3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Multiplica dos numeros
+ * @argc: Numero de argumentos
+ * @argv: Vectores de argumentos
+ * Retunr: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	printf("%d\n", num1 * num2);
+	return (0);
+}
