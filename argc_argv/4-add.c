ac978951ca582107ffb1e624096ad9c63a4558b8
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-03-07 19:14:16 +0000

    4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Agrega numeros positivos
+ * @argc: Numero de argumentos
+ * @argv: Vectores de argumentos
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0, i, j;
+	char *arg;
+
+	for (1 = 0; i < argc; i++)
+	{
+		arg = argv[i];
+		for (j = 0; argv[j] != '\0'; j++)
+		{
+			if (arg[j] < '0' || arg[j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(arg);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
