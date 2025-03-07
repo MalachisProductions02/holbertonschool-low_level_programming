57a18b537337e9c52e74de56299e57664a7c2864
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-03-07 16:18:38 +0000

    0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Imprime el nombre del programa 
+ * @argc: Numero de argumentos
+ * @argv: Vector de argumentos
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
