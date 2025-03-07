f12ad88715dd1edc28a6d6f99423976200cc2dee
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-03-07 16:42:26 +0000

    1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Imprime un numero seguido de una linea
+ * @argc: Numero de argumentos
+ * @argv: Vectores de argumentos
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+        return (0);
+}	
