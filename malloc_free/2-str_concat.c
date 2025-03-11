537b5776c23e49140f6fbdb468fc70878cbcffee
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-03-11 02:38:40 +0000

    2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,18 +14,22 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 != NULL)
 	{
-		while (s1[len1] != '\0')
-		{
-			len1++;
-		}
+		s1 = "";
 	}
 
 	if (s2 != NULL)
 	{
-		while (s2[len2] != '\0')
-		{
-			len2++;
-		}
+		s2 = "";
+	}
+
+	while (s1[len1] != '\0')
+	{
+		len1++;
+	}
+
+	while (s2[len2] != '\0')
+	{
+		len2++;
 	}
 
 	concat = malloc((len1 + len2 + 1) * sizeof(char));
