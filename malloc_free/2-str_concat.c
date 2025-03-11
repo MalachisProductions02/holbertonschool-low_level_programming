06e7274458222de10ee038f07e621a416123d5b6
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-03-11 02:29:40 +0000

    2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concad - Concatena dos string
+ * @s1: Primer parametro
+ * @s2: Segundo parametro
+ * Return: Regresa un puntero
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *concat;
+	int i, j, len1 = 0, len2 = 0;
+
+	if (s1 != NULL)
+	{
+		while (s1[len1] != '\0')
+		{
+			len1++;
+		}
+	}
+
+	if (s2 != NULL)
+	{
+		while (s2[len2] != '\0')
+		{
+			len2++;
+		}
+	}
+
+	concat = malloc((len1 + len2 + 1) * sizeof(char));
+	if (concat == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < len1; i++)
+	{
+		concat[i] = s1[i];
+	}
+
+	for (j = 0; j < len2; j++)
+	{
+		concat[i] = s2[i];
+	}
+
+	concat[i] = '\0';
+
+	retunr (concat);
+}
