0165aa80399381aecf75bc22e17747c2e7c301c3
MalachisProductions02 <malachisproductionsnooficial@gmil.com>
2025-03-12 00:56:35 +0000

    2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -10,28 +10,28 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    char *concat;
-    int i, j, len1 = 0, len2 = 0;
+       	char *concat;
+       	int i, j, len1 = 0, len2 = 0;
 
-    if (s1 != NULL)
-        while (s1[len1])
-            len1++;
+       	if (s1 != NULL)
+	       	while (s1[len1])
+		       	len1++;
 
-    if (s2 != NULL)
-        while (s2[len2])
-            len2++;
+       	if (s2 != NULL)
+	       	while (s2[len2])
+		       	len2++;
 
-    concat = malloc(sizeof(char) * (len1 + len2 + 1));
-    if (concat == NULL)
-        return (NULL);
+       	concat = malloc(sizeof(char) * (len1 + len2 + 1));
+       	if (concat == NULL)
+	       	return (NULL);
 
-    for (i = 0; i < len1; i++)
-        concat[i] = s1[i];
+       	for (i = 0; i < len1; i++)
+	       	concat[i] = s1[i];
 
-    for (j = 0; j < len2; j++)
-        concat[i + j] = s2[j];
+       	for (j = 0; j < len2; j++)
+	       	concat[i + j] = s2[j];
 
-    concat[i + j] = '\0';
+       	concat[i + j] = '\0';
 
-    return (concat);
+       	return (concat);
 }
