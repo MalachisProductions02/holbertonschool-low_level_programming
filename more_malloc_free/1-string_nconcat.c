3ea8e6f14d72ce2b488cad05445f072253be3281
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-03-13 02:48:10 +0000

    1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -14,45 +14,29 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len1 = 0, len2 = 0, i, j;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
 	while (s1[len1] != '\0')
-	{
 		len1++;
-	}
 
 	while (s2[len2] != '\0')
-	{
 		len2++;
-	}
 
 	if (n >= len2)
-	{
 		n = len2;
-	}
 
 	new_str = malloc(len1 + n + 1);
 	if (new_str == NULL)
-	{
 		return (NULL);
-	}
 
 	for (i = 0; i < len1; i++)
-	{
 		new_str[i] = s1[i];
-	}
 
 	for (j = 0; j < n; j++)
-	{
 		new_str[i + j] = s2[j];
-	}
 
 	new_str[i + j] = '\0';
 
