a8937cd515a9bb505b7dd14c23d2d2c4cc41cab1
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-03-13 03:19:27 +0000

    2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * calloc - Aparta memoria de un array de acuerdo a nmemb
+ * @nmemb: Parametro
+ * @size: Tamaño de array
+ * Return: nmemb = 0 or size = 0 or failed, then return NULL
+ */
+void *calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i, total;
+	char *p;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	total = nmemb * size;
+	*p = malloc(total);
+       	if (p == NULL)
+       	{
+		return (NULL)
+	}
+
+	for (i = 0; i < total; i++)
+	{
+		p[i] = 0;
+	}
+
+	return (p);
+}	
