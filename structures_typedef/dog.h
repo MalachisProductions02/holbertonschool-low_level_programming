f13cfa4f21a804b37b1a3ed1c12186b2d1706bbb
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-03-14 02:36:19 +0000

    woof

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A dog's basic info
+ * @name: Name of the dog (char *)
+ * @age: Age of the dog (float)
+ * @owner: Owner of the dog (char *)
+ *
+ * Description: Defines a structure with this three members
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*DOG_H*/
