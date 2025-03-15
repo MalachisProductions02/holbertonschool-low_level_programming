0e65b53ef39a64fba5d8da65202de37df60d844b
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-03-14 18:47:26 -0600

    2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -7,24 +7,24 @@
  * @s2: Second string.
  * Return: Pointer to new string, or NULL on failure.
  */
-char *str_concat(char *s1, char *s2)
+char *str_concat(char *s1, char *s2) // Funcion para concatenar dos strings
 {
-	char *concat;
-	int i, j, len1 = 0, len2 = 0;
+	char *concat; // caracter puntero hacia concat
+	int i, j, len1 = 0, len2 = 0; // Entero i, len1 igualado a 0 y len2 igualado a 0
 
-	if (s1 != NULL)
+	if (s1 != NULL) // Si s1 no esta vacio
 	{
-		while (s1[len1])
-			len1++;
+		while (s1[len1]) // Bucle while cuando s1 tenga un valor
+			len1++; // Suma uno a len1
 	}
 
-	if (s2 != NULL)
+	if (s2 != NULL) // Si s2 no esta vacio
 	{
-		while (s2[len2])
-			len2++;
+		while (s2[len2]) // Bucle while cuando s2 tenga un valor
+			len2++; // Suma uno a len2
 	}
 
-	concat = malloc(sizeof(char) * (len1 + len2 + 1));
+	concat = malloc(sizeof(char) * (len1 + len2 + 1)); // 
 	if (concat == NULL)
 		return (NULL);
 
