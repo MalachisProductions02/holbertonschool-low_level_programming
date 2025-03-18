c9b25093778973ce570c54bd2eb4e8ea5bec8726
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-03-17 20:57:13 -0600

    3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,4 +1,6 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entrada
@@ -8,26 +10,32 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2;
-	int (*op_func)(int, int);
+    int num1, num2;
+    int (*op_func)(int, int);
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[3]);
 
-	op_func = get_op_func(argv[2]);
+    if (argv[2][1] != '\0')
+    {
+        printf("Error\n");
+        exit(99);
+    }
 
-	if (op_func == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
+    op_func = get_op_func(argv[2]);
 
-	printf("%d\n", op_func(num1, num2));
-	return (0);
+    if (op_func == NULL)
+    {
+        printf("Error\n");
+        exit(99);
+    }
+
+    printf("%d\n", op_func(num1, num2));
+    return (0);
 }
