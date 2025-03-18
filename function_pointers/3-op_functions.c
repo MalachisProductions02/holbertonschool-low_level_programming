1b8913fb78bb0990108c6ca3633da12c8a10fcf4
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-03-17 20:59:53 -0600

    3

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,44 +1,20 @@
 #include "3-calc.h"
 
-/**
- * op_add - Suma
- * @a: Parametro
- * @b: Parametro
- * Return: La suma de a y b
- */
 int op_add(int a, int b)
 {
-	return a+ b;
+	return (a + b);
 }
 
-/**
- * op_sub - Resta
- * @a: Parametro
- * @b: Parametro
- * Return: La resta de a y b
- */
 int op_sub(int a, int b)
 {
-	return a-b;
+	return (a - b);
 }
 
-/**
- * op_mul - Multiplicacion
- * @a: Parametro
- * @b: Parametro
- * Return: El producto de a y b
- */
 int op_mul(int a, int b)
 {
-	return a * b;
+	return (a * b);
 }
 
-/**
- * op_div - Division
- * @a: Parametro
- * @b: Parametro
- * Return: El resultado de a y b
- */
 int op_div(int a, int b)
 {
 	if (b == 0)
@@ -46,15 +22,9 @@ int op_div(int a, int b)
 		printf("Error\n");
 		exit(100);
 	}
-	return a / b;
+	return (a / b);
 }
 
-/**
- * op_mod - Residuo
- * @a: Parametro
- * @b: Parametro
- * Return: El residuo de la division de a y b
- */
 int op_mod(int a, int b)
 {
 	if (b == 0)
@@ -62,5 +32,5 @@ int op_mod(int a, int b)
 		printf("Error\n");
 		exit(100);
 	}
-	return a % b;
+	return (a % b);
 }
