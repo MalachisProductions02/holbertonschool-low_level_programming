0fafbd90ba7d9dc8732548bb150c7f51f87e3cc5
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-03-18 23:40:22 +0000

    variadic_functions

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,23 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+
+#include <stdarg.h>
+
+/**
+ * struct printer - Estructura para manejar diferentes datos
+ * @symbol: Tipo de dato
+ * @print: Funcion asociada
+ */
+typedef struct printer 
+{
+	char *symbol;
+	void (*print)(va_list arg);
+} printer_t;
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /* VARIADIC_FUNCTIONS_H */
