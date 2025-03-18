bb52a339869466e1ffa7bb087480c230dd2f8cf3
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-03-17 21:03:22 -0600

    3

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,36 +1,66 @@
 #include "3-calc.h"
 
+/**
+ * op_add - Suma dos números.
+ * @a: El primer número.
+ * @b: El segundo número.
+ * Return: El resultado de la suma de a y b.
+ */
 int op_add(int a, int b)
 {
-	return (a + b);
+    return (a + b);
 }
 
+/**
+ * op_sub - Resta dos números.
+ * @a: El primer número.
+ * @b: El segundo número.
+ * Return: El resultado de la resta de a y b.
+ */
 int op_sub(int a, int b)
 {
-	return (a - b);
+    return (a - b);
 }
 
+/**
+ * op_mul - Multiplica dos números.
+ * @a: El primer número.
+ * @b: El segundo número.
+ * Return: El resultado de la multiplicación de a y b.
+ */
 int op_mul(int a, int b)
 {
-	return (a * b);
+    return (a * b);
 }
 
+/**
+ * op_div - Divide dos números.
+ * @a: El primer número.
+ * @b: El segundo número.
+ * Return: El resultado de la división de a entre b.
+ */
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	return (a / b);
+    if (b == 0)
+    {
+        printf("Error\n");
+        exit(100);
+    }
+    return (a / b);
 }
 
+/**
+ * op_mod - Calcula el módulo de dos números.
+ * @a: El primer número.
+ * @b: El segundo número.
+ * Return: El resultado del módulo de a y b.
+ */
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	return (a % b);
+    if (b == 0)
+    {
+        printf("Error\n");
+        exit(100);
+    }
+    return (a % b);
 }
