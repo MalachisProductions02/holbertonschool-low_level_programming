3e9430b344c9b52b9a5efea8b6fbe585c5c87b96
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-04-02 04:41:00 +0000

    main

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -4,6 +4,21 @@
 #include <stdarg.h>
 
 /**
+ * struct list_s - singly_linked_list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struc list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+/**
  * size_t print_list - Functions that prints all elements for a list
  * @list_t: The list
  * @h: Parameter
