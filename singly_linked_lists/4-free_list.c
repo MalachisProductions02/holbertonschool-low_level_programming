e5e4f0b2b5c4f88065d3019ca064401b5160c1f6
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-04-03 02:44:52 +0000

    free list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - Frees a list_t list
+ * @head: Pointer to the head of the list
+ * Description: This function iterates through the list and frees each node,
+ * including the dynamically allocated string
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(tmp);
+	}
+}
