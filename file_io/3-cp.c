df79da255357d48392d6dd8a7308a010ccdb7601
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-04-05 17:41:23 -0600

    cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -20,21 +20,21 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		printf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		printf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		printf(2, "Error: Can't write to %s\n", argv[2]);
 		close(fd_from);
 		exit(99);
 	}
@@ -44,7 +44,7 @@ int main(int argc, char *argv[])
 		n_written = write(fd_to, buffer, n_read);
 		if (n_written != n_read)
 		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			printf(2, "Error: Can't write to %s\n", argv[2]);
 			close(fd_from);
 			close(fd_to);
 			exit(99);
@@ -53,7 +53,7 @@ int main(int argc, char *argv[])
 
 	if (n_read == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		printf(2, "Error: Can't read from file %s\n", argv[1]);
 		close(fd_from);
 		close(fd_to);
 		exit(98);
@@ -61,13 +61,13 @@ int main(int argc, char *argv[])
 
 	if (close(fd_from) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_from);
+		printf(2, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
 
 	if (close(fd_to) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_to);
+		printf(2, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
 
