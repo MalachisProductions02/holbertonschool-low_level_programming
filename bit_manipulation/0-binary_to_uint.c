04ded0afcfc51a34540d84cacc13c89c9f5ba007
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-04-05 00:28:05 +0000

    binary to uint

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Converts a binary string to an unsigned int
+ * @b: A string of 0 and 1 char
+ * Return: THe converted number, or 0 if the string is invalid
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+		result = result * 2 + (*b - '0');
+		b++;
+	}
+
+	return (result);
+}
