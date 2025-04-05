1cdd7da05fc5d7ab0f50bc8a0fe6f2ab0064a9e4
root <malachisproductionsnooficial@gmail.com>
2025-04-05 19:47:09 +0000

    changes saved

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - Reads a text file
+ * @filename: The file's name
+ * @letters: Number of letters to read
+ * Return: The actual number of letters it could be read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letetrs)
+{
+	int fd;
+	ssize_t n_read, n_written;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+
+		close(fd);
+		return (0);
+	}
+
+	n_read = read(fd, buffer, letters);
+	if (n_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	n_written = write(STDOUT_FILENO, buffer, n_read);
+	if (n_written == -1 || n_written != n_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+	return (n_written);
+}
