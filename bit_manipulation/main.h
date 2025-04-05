7062374a422d4ec31486ce8f0ee89ee3d77f58ca
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-04-05 01:39:21 +0000

    main

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -26,6 +26,15 @@ void print_binary(unsigned long int n);
  */
 int get_bit(unsigned long int n, unsigned int index);
 
+
+/**
+ * set_bit - Sets the value of a bit to 1 at a given index
+ * @n: Pointer to the number to modify
+ * @index: Index of the bit to set, starting from 0
+ * Return: 1 if it worked, or -1 if and error ocurred
+ */
+int set_bit(unsigned long int *n, unsigned int index);
+
 int _putchar(char);
 
 #endif /* MAIN_H */
