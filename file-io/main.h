abcaef31e5430a6e49557c1a166ed9f2569988b2
root <malachisproductionsnooficial@gmail.com>
2025-04-05 19:40:16 +0000

    main

---
+++ file-io/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+
+/**
+ * read_textfile - Reads the text file and prints it to the POSIX standard output
+ * @filename: File's name
+ * @letters: Number of letters to read and print
+ * Return: THe actual number of letters read and printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+int _putchar(char);
+
+#endif /* MAIN_H */
