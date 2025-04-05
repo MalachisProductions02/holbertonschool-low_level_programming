224782436f4a37a17876f066f286c14dad3b301c
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-04-05 00:27:35 +0000

    main

---
+++ bit_manipulation/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * void prototype
+ * int prototype
+ * char prototype
+ * unsigned prototype
+ */
+
+unsigned int binary_to_uint(const char *b);
+
+#endif /* MAIN_H */
