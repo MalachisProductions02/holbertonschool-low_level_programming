d2a0a10441faca91301beec7fa8589c134e2a18c
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-04-05 14:22:50 -0600

    main

--- file_io/main.h
+++ file_io/main.h
@@ -16,6 +16,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 
 int _putchar(char);
-int create_file(const char ¨filename, char *text_content);
+int create_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H */
