a5e0ba968e979f9bf14117c651aa4deef8ef7e67
root <malachisproductionsnooficial@gmail.com>
2025-04-05 22:39:41 +0000

    cp

---
+++ file_io/3-cp.c
@@ -0,0 +1,64 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to, n_read, n_written;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
+	}
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | 0_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	if (fd_to == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	while ((n_read = read(fd_from, buffer, 1024)) > 0)
+	{
+		n_written = write(fd_to, buffer, n_read);
+		if (n_written == -1)
+		{
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+
+	if (n_read == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	if (close(fd_from) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %s\n", fd_from);
+		exit(100);
+	}
+
+	if (close(fd_to) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %s\n", fd_to);
+		exit(100);
+	}
+
+	return (0);
+}
