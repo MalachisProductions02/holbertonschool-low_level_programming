7284ba5ddae5a401c8b758e73cc0c0c99742fd0c
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-04-04 18:42:32 -0600

    main

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,13 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * void prototype
- * int prototype
- * char prototype
- * unsigned prototype
- */
+#include <stddef.h>
 
+/**
+ * binary_to_uint - Converts a binary string to an unsigned int
+ * @b: Pointer to a string containing 0 and 1 characters
+ * Return: The converted unsigned int value, or 0 if the string is invalid
+ */
 unsigned int binary_to_uint(const char *b);
 
 #endif /* MAIN_H */
