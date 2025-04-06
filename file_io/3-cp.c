69d93194c89071459fc140aa51c44a57829da556
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-04-05 20:50:58 -0600

    cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0775);
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
