7e77eb73f9f899347fbf9d490394617398a242e0
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-04-05 20:25:54 -0600

    cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -21,12 +21,14 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
+
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
@@ -34,6 +36,7 @@ int main(int argc, char *argv[])
 		close(fd_from);
 		exit(99);
 	}
+
 	while ((rd = read(fd_from, buf, 1024)) > 0)
 	{
 		wr = write(fd_to, buf, rd);
@@ -45,6 +48,7 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
+
 	if (rd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
@@ -52,15 +56,18 @@ int main(int argc, char *argv[])
 		close(fd_to);
 		exit(98);
 	}
+
 	if (close(fd_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
+
 	if (close(fd_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
+
 	return (0);
 }
